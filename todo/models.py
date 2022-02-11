13b26492968dbdd9a8a0e4b6e8eba3dd792c2cab
Jayanta <bjayanta.me@gmail.com>
2022-02-11 18:31:27 +0600

    add fields into todo-model

--- todo/models.py
+++ todo/models.py
@@ -2,4 +2,14 @@ from django.db import models
 
 # Create your models here.
 class Todo(models.Model):
+    STATUS_CHOICES = (
+        ('Done', 'Done'),
+        ('Pending', 'Pending'),
+        ('Running', 'Running')
+    )
+
     label = models.CharField(max_length=100)
+    status = models.CharField(max_length=100, choices=STATUS_CHOICES)
+    reminder
+    created_at
+    updated_at
